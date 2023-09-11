5e2a62861a157d44cb533db0f9e0a2b769135f6e
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-11 14:06:03 +0430

    Add techer class and fix student class issue

--- student.rb
+++ student.rb
@@ -1,15 +1,17 @@
 require './person'
 
-class Student
+class Student < Person
     attr_accessor :classroom
-  def initialize(classroom, name = "Unknown", age = nil, parent_permission: true)
-    @classroom = classroom
-  end
+
+    def initialize(classroom, age, name, parent_permission: true)
+        super(name, age, parent_permission: parent_permission)
+        @classroom = classroom
+      end
 
   def play_hooky
     '¯(ツ)/¯'
   end
 end
-student1 = Student.new("second","mahdi",20)
+student1 = Student.new("mahdi", 20,"second")
 
 puts student1.classroom
---
+++ teacher.rb
@@ -0,0 +1,21 @@
+require './person'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(name, age, specialization, parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+
+teacher1 = Teacher.new("Ms. Smith", 35, "Mathematics")
+
+puts teacher1.name
+puts teacher1.specialization
+puts teacher1.can_use_services?
