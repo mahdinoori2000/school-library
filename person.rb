226925f786d89e716e5589679a5116b955716a62
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-15 14:57:01 +0430

    Delete prints

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
   attr_accessor :name, :age, :rental
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     super()
     @id = Random.rand(1..10_000)
     @name = name
@@ -39,10 +39,3 @@ class Person < Nameable
     end
   end
 end
-
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
