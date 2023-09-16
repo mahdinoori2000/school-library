34c1fdad3bb3618e1e8028dca6c340ef928462ee
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-16 15:36:21 +0430

    Add descriptive method name

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ my_class = App.new
 @list = ['1 - List all books', '2 - List all people', '3 - Create a person', '4 - Create a book',
          '5 - Create a rental', '6 - List all rentals for a given person id', '7 - Exit']
 
-def method(my_class)
+def main_menu(my_class)
   methods = {
     1 => 'list_all_books',
     2 => 'list_all_people',
@@ -22,8 +22,8 @@ def method(my_class)
     exit
   else
     my_class.send(methods[get_input])
-    method(my_class)
+    main_menu(my_class)
   end
 end
 
-method(my_class)
+main_menu(my_class)
