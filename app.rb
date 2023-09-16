5ed982c005944db8991fef8d6bf07d86f389ffda
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-16 14:22:51 +0430

    Fix linter errors

--- app.rb
+++ app.rb
@@ -25,33 +25,38 @@ class App
   def create_person
     puts "\nDo you want to Create a Student (1) or a Teacher (2)?[input the number]:"
     option = gets.chomp.to_i
-
     if option == 1
-      print 'age: '
-      student_age = gets.chomp.to_i
-      print 'name: '
-      student_name = gets.chomp
-      print 'Has parent permission? [Y/N]'
-      classroom = Classroom.new('class 1')
-      parent_permission = gets.chomp.downcase
-      new_student = Student.new(classroom, student_age, student_name)
-      @people << new_student
-      puts "Person created successfully\n\n"
-
+      create_student
     elsif option == 2
-      print 'age: '
-      teacher_age = gets.chomp.to_i
-      print 'name: '
-      teacher_name = gets.chomp
-      print 'Specialization: '
-      specialization = gets.chomp
-      new_teacher = Teacher.new(teacher_name, teacher_age, specialization)
-      @people << new_teacher
-      puts "Person created successfully\n\n"
-
+      create_teacher
     else
       puts 'Invalid Character'
     end
+  end
+
+  def create_student
+    print 'age: '
+    student_age = gets.chomp.to_i
+    print 'name: '
+    student_name = gets.chomp
+    print 'Has parent permission? [Y/N]'
+    classroom = Classroom.new('class 1')
+    gets.chomp.downcase
+    new_student = Student.new(classroom, student_age, student_name)
+    @people << new_student
+    puts "Person created successfully\n\n"
+  end
+
+  def create_teacher
+    print 'age: '
+    teacher_age = gets.chomp.to_i
+    print 'name: '
+    teacher_name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    new_teacher = Teacher.new(teacher_name, teacher_age, specialization)
+    @people << new_teacher
+    puts "Person created successfully\n\n"
   end
 
   def create_rental
@@ -91,7 +96,6 @@ class App
   def list_all_people
     if @people.empty?
       puts "\n\nno person added"
-      puts "------------------------------------------------------------\n\n"
 
     else
       puts '------------------------------------'
@@ -105,8 +109,8 @@ class App
                end
         puts "#{index} - #{role} - Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
-      puts "------------------------------------------------------------\n\n"
     end
+    puts "------------------------------------------------------------\n\n"
   end
 
   def list_all_rentals
